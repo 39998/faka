1d8d67ee314b6193e8253647943af6cde4668f08
Serendipity <112377744+39998@users.noreply.github.com>
2024-02-29 17:21:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 unzip /file/faka/acgshop-latest.zip -d /file
+chmod 777 /file/acg-faka-main/config -R
+chmod 777 /file/acg-faka-main/kernel -R
 mv /usr/local/nginx/conf/rewrite/rewrite.conf /usr/local/nginx/conf/rewrite/rewrite.confb
 mv /file/faka/rewrite.conf /usr/local/nginx/conf/rewrite/rewrite.conf
 rm -rf /file/faka
